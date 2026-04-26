12df0a4357e06bc15dc99150d0dfa323d697cf12
Vvk0105 <vivekbabu0105@gmail.com>
2026-04-26 22:21:38 +0530

    access lifetime to 60min

--- backend/settings.py
+++ backend/settings.py
@@ -162,8 +162,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    # "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
