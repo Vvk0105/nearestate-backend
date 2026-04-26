2ce4d4230813dc4ceae29f59232ef5165ef80f1c
Vvk0105 <vivekbabu0105@gmail.com>
2026-04-26 20:12:35 +0530

    access time reduce to 10sec for testing

--- backend/settings.py
+++ backend/settings.py
@@ -162,7 +162,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    # "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
